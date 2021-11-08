aeffe767bff853bd1af81156a3f092676c5bd3d1
Jahy0 <1471019484@qq.com>
2021-11-09 00:09:47 +0800

    每日变成

---
+++ Add function.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int size(int x, int y)
+{
+	if(x>y)
+	return x;
+	else
+	return y;
+}
+
+int max(int x, int y)
+{
+	return (x>y)?x:y;
+}
---
+++ char function.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char arr1[] = "awsome";
+	char arr2[20] = "************";
+	strcpy(arr2,arr1);
+	printf("%s\n",arr2);
+} 
